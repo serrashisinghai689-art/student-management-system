3f523a8648c67c30cac8c9ad3c47497de791c92d
serrashisinghai689-art <serrashisinghai689@gmail.com>
2026-07-10 11:27:18 +0530

    Create Main.java

---
+++ StudentManagementSystem/ │ ├── src/ │   ├── database/ │   ├── dao/ │   ├── daoimpl/ │   ├── menu/ │   ├── model/ │   ├── service/ │   ├── util/ │ ├── database/ ├── lib/src/util/src/service/src/menu/src/src/Main.java
@@ -0,0 +1,17 @@
+import menu.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===========================================");
+        System.out.println("      STUDENT MANAGEMENT SYSTEM");
+        System.out.println("===========================================");
+
+        Menu menu = new Menu();
+
+        menu.start();
+
+    }
+
+}
